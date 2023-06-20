d88257da6ab6bf46999e59bb9290e7f2303fdc19
TerryW624 <terrywade634@gmail.com>
2023-06-20 14:08:22 -0700

    feat: song model and SongSchema class setup

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from marshmallow import post_load, fields, ValidationError
 from dotenv import load_dotenv
 from os import environ
 
@@ -23,12 +24,33 @@ CORS(app)
 Migrate(app, db)
 
 # Models
-
-
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    artist = db.Column(db.String(255), nullable=False)
+    album = db.Column(db.String(255), nullable=False)
+    release_date = db.Column(db.Date, nullable=False)
+    genre = db.Column(db.String(255), nullable=False)
+    def __repr__(self):
+        return f"{self.title} {self.artist} {self.album} {self.release_date} {self.genre}"
 
 # Schemas
+class SongSchema(ma.Schema):
+    id = fields.Integer(primary_key=True)
+    title = fields.String(required=True)
+    artist = fields.String(required=True)
+    album = fields.String(required=True)
+    release_date = fields.Date(required=True)
+    genre = fields.String(required=True)
+    class Meta:
+        fields = ("id", "title", "artist", "album", "release_date", "genre")
 
+    @post_load
+    def create_song(self, data, **kwargs):
+        return Song(**data)
 
+song_schema = SongSchema()
+songs_schema = SongSchema(many=True)
 
 # Resources
 
